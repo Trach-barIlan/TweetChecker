4f762878177f98047f6cb8c090d1a88d3f2e0599
yonitrach <ytrachtenberg@gmail.com>
2025-04-17 22:44:36 +0300

    Fix Puppeteer launch options to use boolean for headless mode and remove executablePath

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ app.post("/check", async (req, res) => {
   
     try {
       const browser = await puppeteer.launch({
-        headless: "true",
-        executablePath: "/usr/bin/google-chrome",
-        args: ["--no-sandbox", "--disable-setuid-sandbox"]
-      });
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });      
       
       
       const page = await browser.newPage();
